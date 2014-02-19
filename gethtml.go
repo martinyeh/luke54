4d42a1e579e190b7412946b44e8ba585d8fcd32e
Ubuntu <ubuntu@ip-10-254-111-80.us-west-2.compute.internal>
2014-02-19 02:16:54 +0000

    add new handler view

--- gethtml.go
+++ gethtml.go
@@ -15,6 +15,7 @@ import (
 var (
 	port      = flag.Int("p", 8080, "Luke54 Service Port")
 	url	  = flag.String("url", "http://www.luke54.org/category", "Luke54 url")
+	vurl      = flag.String("vurl", "http://www.luke54.org/view", "Luke54 url")
 )
 
 
@@ -40,6 +41,17 @@ type Message struct {
     Fid string `json:"fid"`
 }
 
+type ViewHandler struct {
+
+}
+
+
+type ViewData struct {
+    Html string    `json:"html"`
+    Vid string `json:"vid"`
+}
+
+
 func (hf *ForumHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
 	r.ParseForm()
 	fd := ForumData{}
@@ -63,6 +75,26 @@ func (hf *ForumHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "[%s]", s)
 }
 
+func GetHtml(url string) *bytes.Buffer {
+        res, err := http.Get(url)
+        if err != nil {
+                fmt.Println("http.Get", err)
+                return nil
+        }
+        defer res.Body.Close()
+
+        body, err := ioutil.ReadAll(res.Body)
+        if err != nil {
+                fmt.Println("ioutil.ReadAll", err)
+                return nil
+        }
+
+        reader := bytes.NewBufferString(string(body))
+        
+        return reader 
+}
+
+
 func (hf *CatgHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
 
 	r.ParseForm()                     // Parses the request body
@@ -72,22 +104,7 @@ func (hf *CatgHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
 	url := fmt.Sprintf(*url+"/%s/%s.html", catg, page)
 	fmt.Println(url)	
 
-	res, err := http.Get(url) 
-        if err != nil { 
-                fmt.Println("http.Get", err) 
-                return 
-        } 
-        defer res.Body.Close() 
-         
-        body, err := ioutil.ReadAll(res.Body) 
-        if err != nil { 
-                fmt.Println("ioutil.ReadAll", err) 
-                return 
-        } 
-
-	reader := bytes.NewBufferString(string(body))
-
-	doc, _ := html.Parse(reader)
+	doc, _ := html.Parse(GetHtml(url))
 
 	m := Message{}
 	m.Fid = catg+page
@@ -110,10 +127,6 @@ func (hf *CatgHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
 				}
 			}
 	
-			/*b, err := json.Marshal(m)
-			if err==nil{
-				fmt.Println("b:" , string(b));
-			}*/
 		} else if n.Type == html.ElementNode && n.Data == "a"{
 			for _, attr := range n.Attr{
 				fmt.Println("attr.Val:" , attr.Val);
@@ -141,21 +154,57 @@ func (hf *CatgHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "[%s]", s[:len(s)-1])
 }
 
+func (hf *ViewHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+
+        r.ParseForm()                     // Parses the request body
+        catg := r.Form.Get("catg")
+        page := r.Form.Get("page")
+
+	url := fmt.Sprintf(*vurl+"/%s/%s.html", catg, page)
+        fmt.Println(url)
+	doc, _ := html.Parse(GetHtml(url))
+
+	vd := ViewData{}
+        vd.Vid = catg+page
+
+	content := "<html><head><title></title></head><body>"
+
+	var f func(*html.Node)
+        f = func(n *html.Node) {
+
+                if n.Type == html.ElementNode && n.Data == "div" {
+
+                        for _, attr := range n.Attr{
+                                fmt.Println("attr.Val:" , attr.Val);
+                                if strings.Contains( attr.Val, "itemFullText"){
+					content+= n.Data
+                                }
+                        }
+
+                } 
+
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+                        f(c)
+                }
+	}
+	f(doc)
+
+	content+= "</body></html>"
+	vd.Html = content
+
+	b, _ := json.Marshal(vd)
+        s := fmt.Sprintf("%s", b)
+        fmt.Fprintf(w, "[%s]", s)
+}
+
 
 func main() { 
 	flag.Parse()
 
-        
-        //lenp := len(body) 
-        //if maxp := 60; lenp > maxp { 
-          //      lenp = maxp 
-        //} 
-        //fmt.Println(len(body), string(body[:lenp])) 
-	//fmt.Println(string(body))
-
 
 	http.Handle("/category", &CatgHandler{})
 	http.Handle("/forum", &ForumHandler{})
+	http.Handle("/view", &ViewHandler{})
 
 	fmt.Println("server start...")
 	err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
